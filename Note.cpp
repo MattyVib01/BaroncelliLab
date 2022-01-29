9bed7687688e0136360e88091311a7a22d4cf1ea
MattyVib01 <mattia.baroncelli@stud.unifi.it>
2022-01-29 19:54:35 +0100

    Creazione: classe Note

--- Note.cpp
+++ Note.cpp
@@ -3,3 +3,15 @@
 //
 
 #include "Note.h"
+
+Note::Note(std::string t, std::string tx) :title(t),text(tx){
+}
+
+std::string Note::getTitle() {return title;}
+void Note::setTitle(std::string t) {title=t;}
+
+std::string Note::getText() {return text;}
+void Note::setText(std::string t) {text=t;}
+
+bool Note::isBlocked() {return blocked;}
+void Note::setBlocked(bool b) {blocked=b;}
--- Note.h
+++ Note.h
@@ -4,9 +4,26 @@
 
 #ifndef BARONCELLILAB_NOTE_H
 #define BARONCELLILAB_NOTE_H
+#include <iostream>
 
 
 class Note {
+public:
+    Note(std::string t, std::string tx);
+
+    std::string getTitle();
+    void setTitle(std::string t);
+
+    std::string getText();
+    void setText(std::string t);
+
+    bool isBlocked();
+    void setBlocked(bool b);
+
+private:
+    std::string title;
+    std::string text;
+    bool blocked;
 };
 
 
