0ad5b9c37349b2f5f1f191925bd30ae9a212b8e4
MattyVib01 <mattia.baroncelli@stud.unifi.it>
2022-01-29 19:33:30 +0100

    Inizio progetto

---
+++ Note.cpp
@@ -0,0 +1,5 @@
+//
+// Created by MattyVib01 on 29/01/2022.
+//
+
+#include "Note.h"
---
+++ Note.h
@@ -0,0 +1,13 @@
+//
+// Created by MattyVib01 on 29/01/2022.
+//
+
+#ifndef BARONCELLILAB_NOTE_H
+#define BARONCELLILAB_NOTE_H
+
+
+class Note {
+};
+
+
+#endif //BARONCELLILAB_NOTE_H
