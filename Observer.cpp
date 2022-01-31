78c594698635f70be035e79efb1705855c8eb202
MattyVib01 <mattia.baroncelli@stud.unifi.it>
2022-01-31 17:15:38 +0100

    Creazione: classe Observer

---
+++ Observer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by MattyVib01 on 31/01/2022.
+//
+
+#include "Observer.h"
---
+++ Observer.h
@@ -0,0 +1,14 @@
+//
+// Created by MattyVib01 on 31/01/2022.
+//
+
+#ifndef BARONCELLILAB_OBSERVER_H
+#define BARONCELLILAB_OBSERVER_H
+
+
+class Observer {
+
+};
+
+
+#endif //BARONCELLILAB_OBSERVER_H
