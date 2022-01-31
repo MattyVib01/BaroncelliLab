5880d59183271b0aa84de81a7309b3555a00eb1c
MattyVib01 <mattia.baroncelli@stud.unifi.it>
2022-01-31 23:28:13 +0100

    Inizio Unit Testing

---
+++ Google_tests/NoteTest.cpp
@@ -0,0 +1,9 @@
+//
+// Created by MattyVib01 on 31/01/2022.
+//
+#include "gtest/gtest.h"
+#include "../Note.h"
+TEST(Note,getTitleTest){
+    Note nota("Titolo","Testo");
+    ASSERT_EQ("Titolo",nota.getTitle());
+}
